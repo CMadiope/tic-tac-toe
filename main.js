3ac9b60840e22ea05c2b4ec4469e3d09e034994e
Confidence Madiope <confidencem97@gmail.com>
2022-07-20 12:27:13 +0200

    ADD: logic to handleResultValidation function

--- main.js
+++ main.js
@@ -14,11 +14,41 @@ statusDisplay.innerHTML = currentPlayerTurn();
 
 function handleCellPlayed() {
   gameState[clickedCellIndex] = currentPlayer;
-  clickedCell.innerHTML = currentPlayer
+  clickedCell.innerHTML = currentPlayer;
 }
 
 function handlePlayerChange() {}
-function handleResultValidation() {}
+const winningConditions = [
+  [0, 1, 3],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i <= 7; i++) {
+    const winningCondition = winningConditions[i];
+    let a = gameState[winningCondition[0]];
+    let b = gameState[winningCondition[1]];
+    let c = gameState[winningCondition[2]];
+    if (a === "" || b === "" || c === "") {
+      continue;
+    }
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    statusDisplay.innerHTML = winningMessage();
+    gameActive = false;
+    return;
+  }
+}
 
 function handleCellClick() {
   const clickedCell = clickedCellEvent.target;
